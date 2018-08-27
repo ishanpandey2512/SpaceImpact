750b0ad4ee080028990b54864df2d8d6240f9f5c
vinayakg7091999 <vinayakg709@gmail.com>
2018-08-27 18:43:35 +0530

    Added moving background and spaceship

---
+++ space.js
@@ -0,0 +1,82 @@
+
+var rocket;
+var bg;
+
+function startGame() {
+    shoot = new component(57, 20, "./images/shoot1.png", 154, 292, "image");
+    rocket = new component(164, 80, "./images/myship.png", 10, 260, "image");
+    bg = new component(1300, 600, "./images/stars bg.jpg", 0, 0, "background");
+    GameArea.start();
+}
+
+var GameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 1300;
+        this.canvas.height = 600;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image" || type == "background") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = GameArea.context;
+        if (type == "image" || type == "background") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "background") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        
+        if (this.type == "background") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+    }    
+}
+
+function updateGameArea() {
+    GameArea.clear();
+    bg.speedX = -1;
+    bg.newPos();    
+    bg.update();
+    rocket.newPos();    
+    rocket.update();
+    shoot.newPos();    
+    shoot.update();
+
+}
+
