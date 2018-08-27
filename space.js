0b341015ecd2632a98b2654beed0db06f18e6990
vinayakg7091999 <vinayakg709@gmail.com>
2018-08-28 00:49:51 +0530

    added moving and firinf of spaceship

--- space.js
+++ space.js
@@ -16,8 +16,15 @@ var GameArea = {
         this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(upu, 20);
+
+        window.addEventListener('keydown', function (e) {
+            GameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            GameArea.key = false;
+        })
         },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -59,6 +66,7 @@ function component(width, height, color, x, y, type) {
     }
     this.newPos = function() {
         this.x += this.speedX;
+        this.y += this.speedY;
         
         if (this.type == "background") {
             if (this.x == -(this.width)) {
@@ -66,17 +74,57 @@ function component(width, height, color, x, y, type) {
             }
         }
     }    
+    this.moverocket = function(){
+    if (GameArea.key == 38) 
+    {rocket.speedY = -2; }
+    if (GameArea.key == 40) 
+    {rocket.speedY = 2; }
+    }
+    
+    this.shooting = function(){
+        if (GameArea.key == 32)
+        {shoot.speedX = 12}
+    }
+    this.occurence = function(){
+        this.active = true;
+        if (GameArea.key == 32)
+        {this.x = rocket.width -10;
+        this.y = rocket.y + rocket.height/2 - 10}
+    }
+
+    this.bound = function(){
+        if (this.y < 10) 
+        { rocket.speedY = 0; }
+        if (this.y + this.height > 590) 
+        { rocket.speedY = 0; }
+        
+        }
+
+
+
 }
+
+
+
 
 function updateGameArea() {
     GameArea.clear();
     bg.speedX = -1;
     bg.newPos();    
     bg.update();
+    rocket.moverocket();
     rocket.newPos();    
     rocket.update();
     shoot.newPos();    
     shoot.update();
+    rocket.bound();
+    
 
+}
+
+function upu(){
+    console.log("hi")
+    shoot.shooting();
+    shoot.occurence();
 }
 
